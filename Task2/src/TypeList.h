146142471e85baee776fac3f93280fb512b57794
RayanAbs <rayanabsalyamov0610@gmail.com>
2026-03-21 02:57:02 +0300

    Implemented recursive TypeList queries

--- Task2/src/TypeList.h
+++ Task2/src/TypeList.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
 
 template <typename... Types>
 struct TypeList
@@ -24,4 +25,62 @@ template <typename T, typename... Types>
 struct PushBack<T, TypeList<Types...>>
 {
     using type = TypeList<Types..., T>;
+};
+
+template <typename T, typename List>
+struct Contains;
+
+template <typename T>
+struct Contains<T, TypeList<>>
+{
+    static constexpr bool value = false;
+};
+
+template <typename T, typename Head, typename... Tail>
+struct Contains<T, TypeList<Head, Tail...>>
+{
+    static constexpr bool value = std::is_same_v<T, Head> || Contains<T, TypeList<Tail...>>::value;
+};
+
+template <std::size_t Index, typename List>
+struct TypeAt;
+
+template <typename Head, typename... Tail>
+struct TypeAt<0, TypeList<Head, Tail...>>
+{
+    using type = Head;
+};
+
+template <std::size_t Index>
+struct TypeAt<Index, TypeList<>>
+{
+    static_assert(Index != Index, "TypeAt: index out of range");
+};
+
+template <std::size_t Index, typename Head, typename... Tail>
+struct TypeAt<Index, TypeList<Head, Tail...>>
+{
+    using type = typename TypeAt<Index - 1, TypeList<Tail...>>::type;
+};
+
+template <typename T, typename List>
+struct IndexOf;
+
+template <typename T>
+struct IndexOf<T, TypeList<>>
+{
+    static_assert(!std::is_same_v<T, T>, "IndexOf: type not found in TypeList");
+};
+
+
+template <typename T, typename... Tail>
+struct IndexOf<T, TypeList<T, Tail...>>
+{
+    static constexpr std::size_t value = 0;
+};
+
+template <typename T, typename Head, typename... Tail>
+struct IndexOf<T, TypeList<Head, Tail...>>
+{
+    static constexpr std::size_t value = 1 + IndexOf<T, TypeList<Tail...>>::value;
 };
--- Task2/src/main.cpp
+++ Task2/src/main.cpp
@@ -1,5 +1,6 @@
 #include "TypeList.h"
 #include <type_traits>
+#include <iostream>
 
 using MyList = TypeList<int, double, char>;
 using EmptyList = TypeList<>;
@@ -8,8 +9,13 @@ static_assert(MyList::size == 3, "MyList should contain 3 types");
 static_assert(EmptyList::size == 0, "EmptyList should contain 0 types");
 static_assert(std::is_same_v<PushFront<float, MyList>::type, TypeList<float, int, double, char>>, "PushFront should add float to the front of MyList");
 static_assert(std::is_same_v<PushBack<float, MyList>::type, TypeList<int, double, char, float>>, "PushBack should add float to the back of MyList");
+static_assert(Contains<int, MyList>::value, "MyList should contain int");
+static_assert(!Contains<float, MyList>::value, "MyList should not contain float");
+static_assert(std::is_same_v<TypeAt<1, MyList>::type, double>);
+static_assert(IndexOf<int, MyList>::value == 0);
 
 int main()
 {
+    std::cout << "All static assertions passed!" << std::endl;
     return 0;
 }
