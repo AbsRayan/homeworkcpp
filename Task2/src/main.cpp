24eadbc0f947c19c748229f148911eaa1db676a8
RayanAbs <rayanabsalyamov0610@gmail.com>
2026-03-21 10:09:40 +0300

    Refactor PushFront and PushBack implementations to use detail namespace

--- Task2/src/main.cpp
+++ Task2/src/main.cpp
@@ -5,14 +5,27 @@
 using MyList = TypeList<int, double, char>;
 using EmptyList = TypeList<>;
 
-static_assert(MyList::size == 3, "MyList should contain 3 types");
-static_assert(EmptyList::size == 0, "EmptyList should contain 0 types");
-static_assert(std::is_same_v<PushFront<float, MyList>::type, TypeList<float, int, double, char>>, "PushFront should add float to the front of MyList");
-static_assert(std::is_same_v<PushBack<float, MyList>::type, TypeList<int, double, char, float>>, "PushBack should add float to the back of MyList");
-static_assert(Contains<int, MyList>::value, "MyList should contain int");
-static_assert(!Contains<float, MyList>::value, "MyList should not contain float");
+static_assert(MyList::size == 3);
+static_assert(EmptyList::size == 0);
+
+static_assert(std::is_same_v<PushFront<bool, MyList>::type, TypeList<bool, int, double, char>>);
+static_assert(std::is_same_v<PushBack<bool, MyList>::type, TypeList<int, double, char, bool>>);
+
+static_assert(Contains<int, MyList>::value);
+static_assert(Contains<double, MyList>::value);
+static_assert(!Contains<float, MyList>::value);
+
+static_assert(std::is_same_v<TypeAt<0, MyList>::type, int>);
 static_assert(std::is_same_v<TypeAt<1, MyList>::type, double>);
+static_assert(std::is_same_v<TypeAt<2, MyList>::type, char>);
+
 static_assert(IndexOf<int, MyList>::value == 0);
+static_assert(IndexOf<double, MyList>::value == 1);
+static_assert(IndexOf<char, MyList>::value == 2);
+
+// should fail to compile
+// using Bad1 = TypeAt<3, MyList>::type;              
+// static_assert(IndexOf<float, MyList>::value == 0);
 
 int main()
 {
