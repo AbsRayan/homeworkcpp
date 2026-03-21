5020a341adfa75e18a5b6f737b8ace122bc0f73f
RayanAbs <rayanabsalyamov0610@gmail.com>
2026-03-21 09:53:47 +0300

    Refactor TypeList: Implement Contains, TypeAt, and IndexOf with detail namespaces

--- Task2/src/TypeList.h
+++ Task2/src/TypeList.h
@@ -27,60 +27,86 @@ struct PushBack<T, TypeList<Types...>>
     using type = TypeList<Types..., T>;
 };
 
+namespace detail
+{
+    template <typename T, typename List>
+    struct ContainsImpl;
+
+    template <typename T>
+    struct ContainsImpl<T, TypeList<>>
+    {
+        static constexpr bool value = false;
+    };
+
+    template <typename T, typename Head, typename... Tail>
+    struct ContainsImpl<T, TypeList<Head, Tail...>>
+    {
+        static constexpr bool value = std::is_same_v<T, Head> || ContainsImpl<T, TypeList<Tail...>>::value;
+    };
+}
+
 template <typename T, typename List>
-struct Contains;
-
-template <typename T>
-struct Contains<T, TypeList<>>
+struct Contains
 {
-    static constexpr bool value = false;
+    static constexpr bool value = detail::ContainsImpl<T, List>::value;
 };
 
-template <typename T, typename Head, typename... Tail>
-struct Contains<T, TypeList<Head, Tail...>>
+namespace detail
 {
-    static constexpr bool value = std::is_same_v<T, Head> || Contains<T, TypeList<Tail...>>::value;
-};
+    template <std::size_t Index, typename List>
+    struct TypeAtImpl;
+
+    template <typename Head, typename... Tail>
+    struct TypeAtImpl<0, TypeList<Head, Tail...>>
+    {
+        using type = Head;
+    };
+
+    template <std::size_t Index>
+    struct TypeAtImpl<Index, TypeList<>>
+    {
+        static_assert(Index != Index, "TypeAt: index out of range");
+    };
+
+    template <std::size_t Index, typename Head, typename... Tail>
+    struct TypeAtImpl<Index, TypeList<Head, Tail...>>
+    {
+        using type = typename TypeAtImpl<Index - 1, TypeList<Tail...>>::type;
+    };
+}
 
 template <std::size_t Index, typename List>
-struct TypeAt;
-
-template <typename Head, typename... Tail>
-struct TypeAt<0, TypeList<Head, Tail...>>
+struct TypeAt
 {
-    using type = Head;
+    using type = typename detail::TypeAtImpl<Index, List>::type;
 };
-
-template <std::size_t Index>
-struct TypeAt<Index, TypeList<>>
+namespace detail
 {
-    static_assert(Index != Index, "TypeAt: index out of range");
-};
+    template <typename T, typename List>
+    struct IndexOfImpl;
 
-template <std::size_t Index, typename Head, typename... Tail>
-struct TypeAt<Index, TypeList<Head, Tail...>>
-{
-    using type = typename TypeAt<Index - 1, TypeList<Tail...>>::type;
-};
+    template <typename T>
+    struct IndexOfImpl<T, TypeList<>>
+    {
+        static_assert(!std::is_same_v<T, T>, "IndexOf: type not found in TypeList");
+    };
+
+
+    template <typename T, typename... Tail>
+    struct IndexOfImpl<T, TypeList<T, Tail...>>
+    {
+        static constexpr std::size_t value = 0;
+    };
+
+    template <typename T, typename Head, typename... Tail>
+    struct IndexOfImpl<T, TypeList<Head, Tail...>>
+    {
+        static constexpr std::size_t value = 1 + IndexOfImpl<T, TypeList<Tail...>>::value;
+    };
+}
 
 template <typename T, typename List>
-struct IndexOf;
-
-template <typename T>
-struct IndexOf<T, TypeList<>>
+struct IndexOf
 {
-    static_assert(!std::is_same_v<T, T>, "IndexOf: type not found in TypeList");
-};
-
-
-template <typename T, typename... Tail>
-struct IndexOf<T, TypeList<T, Tail...>>
-{
-    static constexpr std::size_t value = 0;
-};
-
-template <typename T, typename Head, typename... Tail>
-struct IndexOf<T, TypeList<Head, Tail...>>
-{
-    static constexpr std::size_t value = 1 + IndexOf<T, TypeList<Tail...>>::value;
+    static constexpr std::size_t value = detail::IndexOfImpl<T, List>::value;
 };
